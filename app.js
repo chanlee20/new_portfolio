aa983b0177824745282ccc4be648e16b68f3f8f7
chanlee20 <70008295+chanlee20@users.noreply.github.com>
2022-05-13 11:45:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
-tl.to('.text', {y:'0%', duration: 1});
+tl.to('.text', {y:'0%', duration: 1, stagger: 0.05});
+tl.to('.slider', {y:'-100%', duration: 1.5, delay: 0.5});
+tl.to('.beginning', {y: '-100%', duration: 1}, "-=1");
+// tl.fromTo()
