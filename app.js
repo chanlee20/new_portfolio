a57a352d2cfbdb68d819e5e14421eb18242a3a7a
chanlee20 <70008295+chanlee20@users.noreply.github.com>
2022-05-13 11:55:30 +0900

    1

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 tl.to('.text', {y:'0%', duration: 1, stagger: 0.05});
 tl.to('.slider', {y:'-100%', duration: 1.5, delay: 0.5});
 tl.to('.beginning', {y: '-100%', duration: 1}, "-=1");
-// tl.fromTo()
+tl.fromTo('.name', {opacity:0}, {opacity: 1, duration:1});
+tl.fromTo('.pp', {opacity:0}, {opacity: 1, duration:1}, "-=1");
+tl.fromTo('.icons', {opacity:0}, {opacity: 1, duration:1});
+tl.fromTo('.navbar', {opacity:0}, {opacity: 1, duration:1});
