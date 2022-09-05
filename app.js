28a3df22a6a8a8665c2f04bd825bfcaf83512a54
Chan Lee <chan.l1@wustl.edu>
2022-09-05 15:40:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
 tl.to('.text', {y:'0%', duration: 3, stagger: 0.05});
-tl.to('.slider', {y:'-100%', duration: 4.5, delay: 0.5});
+tl.to('.slider', {y:'-100%', duration: 1.5, delay: 0.5});
 tl.to('.beginning', {y: '-100%', duration: 1}, "-=1");
 tl.fromTo('.name', {opacity:0}, {opacity: 1, duration:1});
 tl.fromTo('.pp', {opacity:0}, {opacity: 1, duration:1}, "-=1");
