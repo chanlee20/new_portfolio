3b9fabd609b19520a3604d0904a6c9b1c394f4d0
chanlee20 <70008295+chanlee20@users.noreply.github.com>
2023-05-17 00:42:26 -0500

    update projects

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ tl.fromTo('.name', {opacity:0}, {opacity: 1, duration:1});
 tl.fromTo('.pp', {opacity:0}, {opacity: 1, duration:1}, "-=1");
 tl.fromTo('.icons', {opacity:0}, {opacity: 1, duration:1});
 tl.fromTo('.navbar', {opacity:0}, {opacity: 1, duration:1});
+
+var navbarLinks = document.querySelectorAll('.navbar a');
+
+navbarLinks.forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent the default link behavior
+
+    var targetId = this.getAttribute('href').substring(1);
+
+    var targetElement = document.getElementById(targetId);
+
+    targetElement.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
